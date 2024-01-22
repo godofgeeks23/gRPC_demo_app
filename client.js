a36e3d8d28a94b6dc1e7740c8595e304cb5d66bf
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-01-22 22:16:17 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,3 +1,28 @@
-// this is the client side of the application
+// create a grpc client
 
-// import modules
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const packageDefinition = protoLoader.loadSync('proto/notes.proto', {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    arrays: true
+});
+const notesProto = grpc.loadPackageDefinition(packageDefinition);
+const NoteService = notesProto.NoteService;
+const client = new NoteService('localhost:50051', grpc.credentials.createInsecure());
+// create a note
+const newNote = {
+    title: 'New Note',
+    content: 'Content of the note',
+    date: new Date()
+};
+client.insert(newNote, (error, note) => {
+    if (!error) {
+        console.log('New note created successfully: ', note);
+    } else {
+        console.error(error);
+    }
+});
+
+
