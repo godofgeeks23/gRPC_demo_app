0eeaa93fc821c8dd33c88e665f6eeda73d50fc5b
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-01-31 22:09:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 // create a grpc server
+
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
+const notes = require('./notes');
+
 const packageDefinition = protoLoader.loadSync('proto/notes.proto', {
     keepCase: true,
     longs: String,
     enums: String,
     arrays: true
 });
+
 const notesProto = grpc.loadPackageDefinition(packageDefinition);
 const server = new grpc.Server();
-const notes = require('./notes');
+
 server.addService(notesProto.NoteService.service, notes);
 server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
 console.log('Server running at http:// 127.0 .0 .1 : 50051');
